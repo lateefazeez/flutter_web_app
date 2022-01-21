e3a76310860c0a23589dae8d69ce096274c08672
Lateef Azeez <codelate18@gmail.com>
2022-01-21 09:40:57 -0700

    initial codes

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,64 @@
+import 'package:web_app/widgets/bottom_bar.dart';
+import 'package:web_app/widgets/carousel.dart';
+import 'package:web_app/widgets/featured_heading.dart';
+import 'package:web_app/widgets/featured_tiles.dart';
+import 'package:web_app/widgets/floating_quick_access_bar.dart';
+import 'package:web_app/widgets/main_heading.dart';
+import 'package:web_app/widgets/menu_drawer.dart';
+import 'package:web_app/widgets/top_bar_contents.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final ScrollController _scrollController = ScrollController();
+  double _scrollPosition = 0;
+  double _opacity = 0;
+
+  _scrollListener() {
+    setState(() {
+      _scrollPosition = _scrollController.position.pixels;
+    });
+  }
+
+  @override
+  void initState() {
+    _scrollController.addListener(_scrollListener);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+    _opacity = _scrollPosition < screenSize.height * 0.40
+        ? _scrollPosition / (screenSize.height * 0.40)
+        : 1;
+
+    return Scaffold(
+      body: Column(
+          children: [
+            Stack(
+              children: [
+                Container(
+                  child: SizedBox(
+                    height: screenSize.height * 0.65,
+                    width: screenSize.width,
+                    child: Image.asset(
+                      'assets/images/background.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+
+              ],
+            ),
+
+          ],
+        ),
+
+    );
+  }
+}
