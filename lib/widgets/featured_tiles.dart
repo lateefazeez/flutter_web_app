37296c19f31db5c38f05b39094ee049867eef0ff
Lateef Azeez <codelate18@gmail.com>
2022-01-21 15:51:37 -0700

    make featured tiles responsive

--- lib/widgets/featured_tiles.dart
+++ lib/widgets/featured_tiles.dart
@@ -19,56 +19,105 @@ class FeaturedTiles extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ResponsiveWidget.isSmallScreen(context)
-        ? Padding(
-            padding: EdgeInsets.only(top: screenSize.height / 50),
-            child: SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  SizedBox(width: screenSize.width / 15),
-                  ...Iterable<int>.generate(assets.length).map(
-                    (int pageIndex) => Row(
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            SizedBox(
-                              height: screenSize.width / 2.5,
-                              width: screenSize.width / 1.5,
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(5.0),
-                                child: Image.asset(
+    return
+      // ResponsiveWidget.isSmallScreen(context) ?
+        // ? Padding(
+        //     padding: EdgeInsets.only(top: screenSize.height / 50),
+        //     child: SingleChildScrollView(
+        //       scrollDirection: Axis.horizontal,
+        //       child: Row(
+        //         mainAxisAlignment: MainAxisAlignment.start,
+        //         children: [
+        //           SizedBox(width: screenSize.width / 15),
+        //           ...Iterable<int>.generate(assets.length).map(
+        //             (int pageIndex) => Row(
+        //               children: [
+        //                 Column(
+        //                   crossAxisAlignment: CrossAxisAlignment.start,
+        //                   children: [
+        //                     SizedBox(
+        //                       height: screenSize.width / 2.5,
+        //                       width: screenSize.width / 1.5,
+        //                       child: ClipRRect(
+        //                         borderRadius: BorderRadius.circular(5.0),
+        //                         child: Image.asset(
+        //                           assets[pageIndex],
+        //                           fit: BoxFit.cover,
+        //                         ),
+        //                       ),
+        //                     ),
+        //                     Padding(
+        //                       padding: EdgeInsets.only(
+        //                         top: screenSize.height / 70,
+        //                       ),
+        //                       child: Text(
+        //                         title[pageIndex],
+        //                         style: TextStyle(
+        //                           fontSize: 16,
+        //                           fontFamily: 'Montserrat',
+        //                           fontWeight: FontWeight.w500,
+        //                         ),
+        //                       ),
+        //                     ),
+        //                   ],
+        //                 ),
+        //                 SizedBox(width: screenSize.width / 15),
+        //               ],
+        //             ),
+        //           ),
+        //         ],
+        //       ),
+        //     ),
+        //   )
+        screenSize.width < 800 ? Padding(
+          padding: EdgeInsets.only(
+            top: screenSize.height / 50,
+          ),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                SizedBox(width: screenSize.width / 15,),
+            ...Iterable<int>.generate(assets.length).map(
+                  (int pageIndex) => Row(
+                    children: [
+                      Column(
+                        children: [
+                          SizedBox(
+                            height: screenSize.width / 2.5,
+                            width: screenSize.width / 1.5,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(5.0),
+                              child: Image.asset(
                                   assets[pageIndex],
                                   fit: BoxFit.cover,
-                                ),
                               ),
                             ),
-                            Padding(
+                          ),
+                          Padding(
                               padding: EdgeInsets.only(
-                                top: screenSize.height / 70,
+                                top: screenSize.height / 70
                               ),
-                              child: Text(
-                                title[pageIndex],
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontFamily: 'Montserrat',
-                                  fontWeight: FontWeight.w500,
-                                ),
+                            child: Text(
+                              title[pageIndex],
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontFamily: "Montserrat",
+                                fontWeight: FontWeight.w500,
                               ),
                             ),
-                          ],
-                        ),
-                        SizedBox(width: screenSize.width / 15),
-                      ],
-                    ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(width: screenSize.width / 15,)
+                    ],
                   ),
-                ],
-              ),
             ),
-          )
-        : Padding(
+              ],
+            ),
+          ),
+        ):
+        Padding(
             padding: EdgeInsets.only(
               top: screenSize.height * 0.06,
               left: screenSize.width / 15,
